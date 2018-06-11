282141461d9a01546ce53d6ed3749359df5d8735
yuan hai zhao <zhaoyuanhai66@outlook.com>
2018-06-11 16:35:57 +0800

    init

---
+++ dist/scripts/index.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var vue_1 = require("vue");
+new vue_1["default"]({
+    el: "#app",
+    data: {
+        name: String
+    },
+    methods: {
+        add: function () {
+            this.name.toString();
+        },
+        click: function () {
+        }
+    }
+});
+//# sourceMappingURL=index.js.map
---
+++ scripts/index.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+new Vue({
+    el: "#app",
+    data: {
+        name: String
+    },
+    methods: {
+        add() {
+            this.name.toString(); 
+        },
+        click(){
+
+        }
+    }
+});
